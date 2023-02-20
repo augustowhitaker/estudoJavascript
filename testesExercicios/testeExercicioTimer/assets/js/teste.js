eb4964cc5fb4b4a1805f453b12141ec35216ece5
augustowhitaker <awhitakerflx@gmail.com>
2023-02-20 02:47:28 -0300

    teste1

--- testesExercicios/testeExercicioTimer/assets/js/teste.js
+++ testesExercicios/testeExercicioTimer/assets/js/teste.js
@@ -1,43 +1,45 @@
-
-function escopo(){
-    function pegaHoraSegundos(segundos){
-
-        const data = new Date(segundos * 1000)
-        return data.toLocaleTimeString('pt-BR',{
+function meuEscopo(){
+    function pegaHorasPorSegundos(segundos){
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
             hour12: false,
             timeZone: 'UTC'
         });
+
     }
+        const relogio = document.querySelector('.relogio');
+        let segundos = 0
+        let timer;
 
-    const relogio = document.querySelector('.relogio');
-    let segundos = 0;
-    let timer;
+        function iniciaRelogio(){
+            timer = setInterval(function(){
+                segundos++
+                relogio.innerHTML = pegaHorasPorSegundos(segundos);
 
-    function iniciaRelogio(){
-        timer = setInterval(function (){
-            segundos++
-            relogio.innerHTML = pegaHoraSegundos(segundos)
-        }, 1000);
-    }
-
-    document.addEventListener('click', (e) => {
-        const el = e.target
-
-        if(el.classList.contains('iniciar')){
-            clearInterval(timer);
-            relogio.classList.remove('pausado')
-            iniciaRelogio();
+            }, 1000);
         }
-        if(el.classList.contains('pausar')){
-            clearInterval(timer);
-            relogio.classList.add('pausado');
-        }
-        if(el.classList.contains('zerar')){
-            clearInterval(timer)
-            relogio.classList.remove('pausado')
-            relogio.innerHTML = '00:00:00'
-        }
-    });
-    
+
+        document.addEventListener('click', (e) => {
+            const el = e.target;
+
+            if(el.classList.contains('iniciar')){
+                clearInterval(timer);
+                relogio.classList.remove('pausado')
+                iniciaRelogio();
+            }
+
+            if(el.classList.contains('zerar')){
+                clearInterval(timer);
+                relogio.classList.remove('pausado');
+                relogio.innerHTML = '00:00:00'
+                segundos = 0;
+            }
+
+            if(el.classList.contains('pausar')){
+                clearInterval(timer)
+                relogio.classList.add('pausado')
+            }
+        });
+        
 }
-escopo();
+meuEscopo();
